8036abac307ee7192bf9ec553489138db1f49e69
baileytye <baileytye@users.noreply.github.com>
2019-03-10 21:42:37 -0600

    Add TMDB file (somehow was removed)

---
+++ app/src/main/java/com/tye/popularmovies/TMDBApi.java
@@ -0,0 +1,30 @@
+package com.tye.popularmovies;
+
+
+import com.tye.popularmovies.Models.MovieResults;
+import com.tye.popularmovies.Models.ReviewResults;
+import com.tye.popularmovies.Models.TrailerResults;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
+public interface TMDBApi {
+
+    String API_KEY = BuildConfig.API_KEY;
+
+    @GET("3/movie/popular?api_key=" + API_KEY)
+    Call<MovieResults> getMoviesPopular();
+
+    @GET("3/movie/top_rated?api_key=" + API_KEY)
+    Call<MovieResults> getMoviesRatings();
+
+    @GET("3/movie/{id}/reviews?api_key=" + API_KEY)
+    Call<ReviewResults> getReviews(@Path("id") int id);
+
+    @GET("3/movie/{id}/videos?api_key=" + API_KEY)
+    Call<TrailerResults> getTrailers(@Path("id") int id);
+
+
+
+}
